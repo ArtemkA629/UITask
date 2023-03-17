219452338822f7b9f93d5724b7b193c300aaa02c
ArtemkA629 <deffing9@mail.ru>
2023-03-17 21:40:14 +0300

    commit1

---
+++ Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+
+    [SerializeField] private List<Renderer> _rendererList;
+
+    public void SetMaterial(Material material) 
+    {
+        foreach (var renderer in _rendererList)
+            renderer.material = material;
+    }
+
+}
